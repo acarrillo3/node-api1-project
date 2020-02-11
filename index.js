054ce3556db792262dd985d1a7126f97f5662327
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-02-10 22:04:15 -0500

    completed POST req

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // implement your API here
+const express = require("express");
+const Users = require("./data/db.js");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ name: "Ana" });
+});
+
+// add user
+server.post("/api/users", (req, res) => {
+  console.log(req.body);
+  if (!req.body.name || !res.body.bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    const newUser = req.body;
+    Users.insert(newUser)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({
+            errorMessage:
+              "There was an error while ssaving the user to the database"
+          });
+      });
+  }
+});
+
+const port = 5000;
+server.listen(port, () => console.log("Server is running on port", port));
